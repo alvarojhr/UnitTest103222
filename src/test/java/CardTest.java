536544ec0078962c98663aad6a3ab95082d1a654
JPabloCarvajal <RatatoskrBRUH@gmail.com>
2023-10-19 20:26:20 -0500

    Tescard

---
+++ src/test/java/CardTest.java
@@ -0,0 +1,18 @@
+import models.Card;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class CardTest {
+    @Test
+    void testgetRank() {
+        Card card = new Card(Card.Suit.CLUBS, Card.Rank.KING);
+        assertEquals(Card.Rank.KING,card.getRank());
+
+    }
+    @Test
+    void TestToString (){
+        Card card = new Card(Card.Suit.CLUBS, Card.Rank.KING);
+        assertEquals(Card.Rank.KING+ " "+ Card.Suit.CLUBS,card.toString());
+    }
+}
+
